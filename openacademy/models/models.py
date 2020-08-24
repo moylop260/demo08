cc6f6c707b2941f2e7689dd0d00a8f122639448b
Demian Cortes <demiancortes91@gmail.com>
2020-08-24 17:17:41 -0600

    [ADD] openacademy: Use menu, xml files, basic fields

---
+++ openacademy/models/models.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class Course(models.Model):
+      _name = 'openacademy.course'
+      _description = 'Model to store course'
+
+      name = fields.Char(String="Title", required=True)
+      description = fields.Text()
