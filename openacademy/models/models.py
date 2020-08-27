8d0655748aeb9bc69f5717a6b12af32de1d71d06
Fausto Rodriguez <faustorguez@gmail.com>
2020-08-27 03:26:33 -0600

    [REF] openacademy: Add tree, form, search views

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -5,6 +5,20 @@ from odoo import models, fields, api
 
 class Course(models.Model):
     _name = 'openacademy.course'
-    _description = 'Model to store course'
+    _description = 'Clase o modulo para definir cursos'
+        
     name = fields.Char(string="Title-F", required=True)
     description = fields.Text()
+    responsible_id = fields.Many2one('res.users', string="Responsible", index=True, ondelete='set null')
+    sessions_ids = fields.One2many('openacademy.session', 'course_id')
+    
+class Session(models.Model):
+    _name = 'openacademy.session'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6,2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+    instructor_id = fields.Many2one('res.partner', string='Instructor')
+    course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees") 
