e09dd168cfb00e9172e76e049ea377e1b8f9df27
Fausto Rodriguez <faustorguez@gmail.com>
2020-09-02 20:50:31 -0600

    [REF] openacademy: Add inherit matter

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -14,11 +14,12 @@ class Course(models.Model):
     
 class Session(models.Model):
     _name = 'openacademy.session'
-
+    _description = 'Clase o modulo para definir Sessions'   
+       
     name = fields.Char(required=True)
     start_date = fields.Date()
     duration = fields.Float(digits=(6,2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    instructor_id = fields.Many2one('res.partner', string='Instructor')
+    instructor_id = fields.Many2one('res.partner', string='Instructor', domain=['|', ('instructor', '=', True), ('category_id.name', 'ilike', "Teacher")])
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
-    attendee_ids = fields.Many2many('res.partner', string="Attendees") 
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
