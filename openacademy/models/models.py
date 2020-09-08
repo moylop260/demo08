e339621a4cb1e2e7f082f9892fd514e936d17b6b
Fausto Rodriguez <faustorguez@gmail.com>
2020-09-08 01:44:12 -0600

    [REF] advanced views

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -2,6 +2,8 @@
 
 from odoo import models, fields, api, exceptions
 import time
+from psycopg2 import IntegrityError
+from datetime import timedelta
 
 def get_uid(self, *a):
     return self.env.uid
@@ -56,7 +58,10 @@ class Course(models.Model):
             new_name = "Copy of %s (%s)"% (self.name, copied_count)
        #default['name'] = self.name + ' otro'
         default['name'] = new_name
+        #try:
         return super(Course, self).copy(default)
+        #except IntegrityError:
+
 
 
 class Session(models.Model):
@@ -71,14 +76,18 @@ class Session(models.Model):
     duration = fields.Float(digits=(6,2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     active = fields.Boolean(default=True)
+    color = fields.Integer()
 
     instructor_id = fields.Many2one('res.partner', string='Instructor', domain=['|', ('instructor', '=', True), ('category_id.name', 'ilike', "Teacher")])
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
-    #taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
-    taken_seats = fields.Float(compute='_taken_seats')
-    active = fields.Boolean(default=True)
+   #taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    taken_seats = fields.Float(compute='_taken_seats', store=True)
+    active      = fields.Boolean(default=True)
+    end_date    = fields.Date( string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
+    hours       = fields.Float(string="Duration in hours", compute='_get_hours', inverse='_set_hours')
+    attendees_count = fields.Integer(string="Attendees count", compute='_get_attendees_count', store=True)
 
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
@@ -112,6 +121,43 @@ class Session(models.Model):
             }
         self.active = True
 
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            start = fields.Datetime.from_string(r.start_date)
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = start + duration
+
+    def _set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+
+            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # so add one day to get 5 days instead
+            start_date = fields.Datetime.from_string(r.start_date)
+            end_date = fields.Datetime.from_string(r.end_date)
+            r.duration = (end_date - start_date).days + 1
+
+    @api.depends('duration')
+    def _get_hours(self):
+        for r in self:
+            r.hours = r.duration * 24
+
+    def _set_hours(self):
+        for r in self:
+            r.duration = r.hours / 24
+
+    @api.depends('attendee_ids')
+    def _get_attendees_count(self):
+        for r in self:
+            r.attendees_count = len(r.attendee_ids)
 
     @api.constrains('instructor_id', 'attendee_ids')
     def _check_instructor_not_in_attendees(self):
