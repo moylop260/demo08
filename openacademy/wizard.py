0e97f10857d7160fe9667c1203481f427b2f16c7
Fausto Rodriguez <faustorguez@gmail.com>
2020-09-09 22:40:21 -0600

    [REF] openacademy: Add wizard

---
+++ openacademy/wizard.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Wizard(models.TransientModel):
+    _name = 'openacademy.wizard'
+    _description = 'Clase o modulo para definir Wizard'
+
+    def _default_session(self):
+        return self.env['openacademy.session'].browse(self._context.get('active_ids'))
+
+    session_ids = fields.Many2many('openacademy.session',
+        string="Session", required=True, default=_default_session)        
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+    def subscribe(self):
+        for session in self.session_ids:
+            session.attendee_ids |= self.attendee_ids
+        return {}
