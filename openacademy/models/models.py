44fdc646f8986fabb6fa9ee435f4d8eea8a5fb53
Moisés López <moylop260@vauxoo.com>
2020-10-07 15:20:30 +0000

    [REF] openacademy: Add newlines

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -5,8 +5,10 @@ import time
 from psycopg2 import IntegrityError
 from datetime import timedelta
 
+
 def get_uid(self, *a):
 	return self.env.uid
+
 
 class Course(models.Model):
 	_name = 'openacademy.course'
